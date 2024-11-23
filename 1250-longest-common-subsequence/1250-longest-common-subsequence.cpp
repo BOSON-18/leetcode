1a47f6edf41eeeee9f18f61f89a49fc36f6db5a4
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2024-11-23 17:37:26 +0530

    Time: 22 ms (81.37%), Space: 26.1 MB (30.18%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -18,25 +18,33 @@ int memo(int ind1,string& text1,int ind2,string& text2,  vector<vector<int>>& dp
 
        //nsae case 
        //right shift indexs 0=1 1=2 so insetead of i<0 i==0 will be abse case and will return 0
-       for(int i=0;i<=n;i++){
-        dp[i][0]=0;
+    //    for(int i=0;i<=n;i++){
+    //     dp[i][0]=0;
+    //     // dp[i][0]=0;
+    //    }
+
+    vector<int> prev(m+1,0),curr(m+1,0);
+       for(int j=0;j<=m;j++){
+        prev[j]=0;
+        // dp[0][j]=0;
        }
 
-       for(int j=0;j<=m;j++){
-        dp[0][j]=0;
-       }
 
        //convert to for loops in opposite manner of recurrence
 
        for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             if(text1[i-1]==text2[j-1])
-                dp[i][j]=1+dp[i-1][j-1];
+                curr[j]=1+prev[j-1];
+                // dp[i][j]=1+dp[i-1][j-1];
             else
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);    
+                curr[j]=max(prev[j],curr[j-1]);    
+                // dp[i][j]=max(dp[i-1][j],dp[i][j-1]);    
         }
+        prev=curr;
        }
 
-       return dp[n][m];
+       return prev[m];
+    //    return dp[n][m];
     }
 };
