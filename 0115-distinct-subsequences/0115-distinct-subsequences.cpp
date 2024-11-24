73974e0cce066458fd5c24d0f94ac0dc24e47d4f
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2024-11-24 13:55:37 +0530

    Time: 30 ms (66.28%), Space: 43 MB (32.66%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -16,9 +16,28 @@ public:
     } 
     int numDistinct(string s, string t) {
 
-        vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
+        vector<vector<double>>dp(s.size()+1,vector<double>(t.size()+1,0));// for tabulation
+        // vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
 
-        return memo(s.size()-1,s,t.size()-1,t,dp);
-        
+        for(int i=0;i<=s.size();i++){
+            dp[i][0]=1;
+        }
+
+        for(int j=1;j<=t.size();j++){
+            dp[0][j]=0;
+        }
+
+        for(int i=1;i<=s.size();i++){
+            for(int j=1;j<=t.size();j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else 
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+
+        // return memo(s.size()-1,s,t.size()-1,t,dp);
+        return (int)dp[s.size()][t.size()];
     }
 };
