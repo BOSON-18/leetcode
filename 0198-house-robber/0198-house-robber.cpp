41cce8678b3b118606c8f341364eb7e7e833fdf9
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2024-12-19 17:27:54 +0530

    Time: 0 ms (100%), Space: 10.2 MB (26.29%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,9 +16,12 @@ public:
 
         // vector<int> dp(nums.size(),-1);
     int n=nums.size();
-    vector<int>dp(n,0);
+    // vector<int>dp(n,0);
 
-       dp[0] = nums[0];
+    //    dp[0] = nums[0];
+
+       int prev2=0;
+       int prev=nums[0];
     
     // Iterate through the elements of the numsay
     for (int i = 1; i < n; i++) {
@@ -26,14 +29,20 @@ public:
         // or not picking it (i.e., taking the maximum of dp[i-2] + nums[i] and dp[i-1])
         int pick = nums[i];
         if (i > 1)
-            pick += dp[i - 2];
-        int nonPick = dp[i - 1];
+            // pick += dp[i - 2];
+            pick += prev2;
+        // int nonPick = dp[i - 1];
+        int nonPick = prev;
         
         // Store the maximum value in the dp numsay
-        dp[i] = max(pick, nonPick);
+        int curr = max(pick, nonPick);
+
+        prev2=prev;
+        prev=curr;
     }
     
     // The last element of the dp numsay will contain the maximum sum
-    return dp[n - 1];
+    // return dp[n - 1];
+    return prev;
     }
 };
