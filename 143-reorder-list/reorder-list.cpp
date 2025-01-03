0dff040303fb191aace9d1f3a59b622915822aa5
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2025-01-03 17:02:11 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (69.51%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -10,49 +10,92 @@
  */
 class Solution {
 public:
+void f(ListNode* head){
+    stack<ListNode*> st;
+
+        ListNode* temp = head;
+        while (temp) {
+            st.push(temp);
+            temp = temp->next;
+        }
+        ListNode* curr = head;
+        unordered_map<ListNode*, bool> vis;
+
+        while (true) {
+            ListNode* last = st.top();
+            st.pop();
+
+            ListNode* front = curr->next;
+            vis[curr] = true;
+            if (vis[last]) {
+                // Cycle create hori traversal complete ho cvhuka
+                // Infonite loop ko break krne ke liye creak kro cycle
+                curr->next = NULL;
+                break;
+            }
+            curr->next = last;
+            vis[last] = true;
+            curr = curr->next;
+            if (vis[front]) {
+                curr->next = NULL;
+                break;
+            }
+
+            curr->next = front;
+            curr = curr->next;
+        }
+}
     void reorderList(ListNode* head) {
         // Given a list
         // 1->2->3->4
-        //0,1,2,3
-        //make it 0,n-i,1,n-i
+        // 0,1,2,3
+        // make it 0,n-i,1,n-i
 
         // take a stack for storing purpose
 
-        stack<ListNode*> st;
+        //  f(ListNode* head);
 
-        ListNode* temp=head;
-        while(temp){
-            st.push(temp);
-            temp=temp->next;
-        }
-        ListNode* curr=head;
-        unordered_map<ListNode*,bool> vis;
+        //  Now to optimise it 
+        // No extra space
 
-      while(true){
-        ListNode* last=st.top();
-        st.pop();
+       if (!head || !head->next) return; // Edge case: empty or single-node list
 
-        ListNode* front=curr->next;
-        vis[curr]=true;
-        if(vis[last]){
-            // Cycle create hori traversal complete ho cvhuka 
-            // Infonite loop ko break krne ke liye creak kro cycle
-            curr->next=NULL;
-            break;
-        }
-        curr->next=last;
-        vis[last]=true;
-        curr=curr->next;
-        if(vis[front]){
-            curr->next=NULL;
-            break;
+        // Find the middle of the list
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        curr->next=front;
-        curr=curr->next;
-      }
+        // Split into two halves and reverse the second half
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = slow->next;
+        slow->next = nullptr;
+
+        ListNode* prev = nullptr;
+        while (secondHalf) {
+            ListNode* temp = secondHalf->next;
+            secondHalf->next = prev;
+            prev = secondHalf;
+            secondHalf = temp;
+        }
+        secondHalf = prev;
+
+        // Merge the two halves
+        while (firstHalf && secondHalf) {
+            ListNode* temp1 = firstHalf->next;
+            ListNode* temp2 = secondHalf->next;
+
+            firstHalf->next = secondHalf;
+            secondHalf->next = temp1;
+
+            firstHalf = temp1;
+            secondHalf = temp2;
+
+        }
 
         // return head;
-        
     }
 };
