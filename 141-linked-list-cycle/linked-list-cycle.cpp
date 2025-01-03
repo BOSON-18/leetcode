84663dc6cede604d0e359a58c51c8156cb10d293
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2025-01-03 15:05:51 +0530

    Time: 8 ms (76.08%) | Memory: 11.9 MB (19.95%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -11,16 +11,36 @@ public:
     bool hasCycle(ListNode *head) {
         //Brute Force 
     // Will use a map structure to store if the node visited or not
-    unordered_map<ListNode*,bool> mpp;
+    // unordered_map<ListNode*,bool> mpp;
 
-    ListNode* temp=head;
-    while(temp){
-        if(mpp[temp]) return true;
-        mpp[temp]=true;
-        temp=temp->next;
+    // ListNode* temp=head;
+    // while(temp){
+    //     if(mpp[temp]) return true;
+    //     mpp[temp]=true;
+    //     temp=temp->next;
+    // }
+
+    // return false;
+
+    // Now What if O(1) space???
+    // Algoruthm-->Slow / Fast
+
+    ListNode* slow=head;
+    ListNode* fast=head;
+    // Ek circle me Running kr rhe me aur nilay 
+    // Deeshank Speed= x Nilay Speed= 2x
+    // Obivously There will be a point when Deeshank will complete the first round Nilay will be completing his 2 round 
+    // They are meeting because running in a circular grond
+    // Deeshank Slow Nilay Fast
+
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow== fast) return true;
+
     }
 
-    return false;
-        
+
+       return false; 
     }
 };
