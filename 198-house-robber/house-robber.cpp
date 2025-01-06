7ceb0324d4a9d34ff9516ca13f11a8913ce665eb
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2025-01-07 00:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (15.87%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
+    int memo(vector<int>& arr, int index, vector<int>& dp) {
+        if (index >= arr.size())
+            return 0;
+        if (dp[index] != -1)
+            return dp[index];
 
-int memo(vector<int>& arr,int index,vector<int>& dp){
-    if(index>=arr.size()) return 0;
-    if(dp[index]!=-1) return dp[index];
+        // take or not take
 
+        int take = arr[index] + memo(arr, index + 2, dp);
+        int notTake = 0 + memo(arr, index + 1, dp);
 
-    // take or not take
-
-    int take=arr[index]+memo(arr,index+2,dp);
-    int notTake=0+memo(arr,index+1,dp);
-
-    return dp[index]=max(take,notTake);
-}
+        return dp[index] = max(take, notTake);
+    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return memo(nums,0,dp);
-        
+        // vector<int> dp(nums.size(),-1);
+        vector<int> dp(nums.size() + 1, 0);
+        // return memo(nums,0,dp);
+        int n = nums.size();
+        dp[n] = 0;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            int take = nums[i] ;
+            if(i<n-1)
+            take+= dp[i + 2];
+            int notTake = dp[i + 1];
+
+            dp[i] = max(take, notTake);
+        }
+        return dp[0];
     }
 };
