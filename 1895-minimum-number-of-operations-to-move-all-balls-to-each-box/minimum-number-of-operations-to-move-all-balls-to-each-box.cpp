f95fb329483653648414c19b5dda4af2e6f6b72c
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2025-01-06 15:25:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (50.97%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,20 +1,45 @@
 class Solution {
 public:
     vector<int> brute(string boxes) {
-        int n=boxes.size();
-        vector<int> ans(n,0);
+        int n = boxes.size();
+        vector<int> ans(n, 0);
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(boxes[j]=='1') ans[i]+=abs(j-i);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (boxes[j] == '1')
+                    ans[i] += abs(j - i);
             }
         }
 
         return ans;
     }
 
+    vector<int> optimal(string boxes) {
+        int n = boxes.size();
+        vector<int> answer(n, 0);
+
+        int cumValue = 0,  cumValueSum = 0;
+
+        // Find Moves for all left balls to index i
+        for (int i = 0; i < n; i++) {
+            answer[i] = cumValueSum;
+            cumValue += boxes[i] == '0' ? 0 : 1;
+            cumValueSum += cumValue;
+        }
+        cumValue = 0;
+        cumValueSum = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] += cumValueSum;
+            cumValue += boxes[i] == '0' ? 0 : 1;
+            cumValueSum += cumValue;
+        }
+
+        return answer;
+    }
+
     vector<int> minOperations(string boxes) {
         // BruteForce
-        return brute(boxes);
+        // return brute(boxes);
+        return optimal(boxes);
     }
 };
