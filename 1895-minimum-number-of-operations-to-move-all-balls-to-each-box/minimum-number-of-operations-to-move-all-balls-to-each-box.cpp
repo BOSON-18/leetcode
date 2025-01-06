f0d327d207f03966516263ed130e97967ff25771
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2025-01-06 13:39:39 +0530

    Time: 127 ms (8.32%) | Memory: 12.1 MB (67.01%) - LeetSync

---
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> brute(string boxes) {
+        int n=boxes.size();
+        vector<int> ans(n,0);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(boxes[j]=='1') ans[i]+=abs(j-i);
+            }
+        }
+
+        return ans;
+    }
+
+    vector<int> minOperations(string boxes) {
+        // BruteForce
+        return brute(boxes);
+    }
+};
