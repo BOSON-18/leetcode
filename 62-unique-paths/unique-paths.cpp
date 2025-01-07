2326d22931ee1b820c2b81e61e3187e5c0bbb863
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2025-01-07 16:08:36 +0530

    Time: 3 ms (6.83%) | Memory: 9.3 MB (40.07%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -15,7 +15,18 @@ int memo(int m,int n,int row,int col){
     int uniquePaths(int m, int n) {
         // Only down and right 
         // i +1 || j+1  at a time bound check krna
-        memset(t,-1,sizeof(t));
-        return memo(m,n,0,0);
+        // memset(t,-1,sizeof(t));
+
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[m-1][n-1]=1;
+
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i+1<m) dp[i][j]+=dp[i+1][j];
+                if(j+1<n) dp[i][j]+=dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+        // return memo(m,n,0,0);
     }
 };
