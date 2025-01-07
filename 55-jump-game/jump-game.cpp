f45059182783be88a1884597cf32e5ff0f86ebf5
batraDeeshank <111277323+BOSON-18@users.noreply.github.com>
2025-01-07 16:43:12 +0530

    Time: 271 ms (15.08%) | Memory: 53.2 MB (14.12%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-int n;
+    int n;
 
-bool memo(vector<int>& nums,int index,vector<int>& dp){
+    bool memo(vector<int>& nums, int index, vector<int>& dp) {
 
-    if(index==n-1) return true;
+        if (index == n - 1)
+            return true;
 
-    if(dp[index]!=-1) return dp[index];
+        if (dp[index] != -1)
+            return dp[index];
 
-    if(nums[index]==0) return dp[index]=false;
+        if (nums[index] == 0)
+            return dp[index] = false;
 
-    for(int i=1;i<=nums[index];i++){
-        if(memo(nums,index+i,dp)) return dp[index]=true;
+        for (int i = 1; i <= nums[index]; i++) {
+            if (memo(nums, index + i, dp))
+                return dp[index] = true;
+        }
+        return dp[index] = false;
     }
-    return dp[index]=false;
-}
     bool canJump(vector<int>& nums) {
-        n=nums.size();
-        vector<int> dp(n,-1);
+        n = nums.size();
+        // vector<int> dp(n, -1);
+        vector<bool> dp(n, 0);
+        dp[n-1]=1;
 
-        return memo(nums,0,dp);
-        
+        for(int i=n-2;i>=0;i--){
+           for(int j=0;j<=nums[i];j++){
+             if (dp[i + j]) {
+                    dp[i] = true;
+                    break; 
+                }
+           }
+        }
+
+
+    return dp[0];
+        // return memo(nums, 0, dp);
     }
 };
