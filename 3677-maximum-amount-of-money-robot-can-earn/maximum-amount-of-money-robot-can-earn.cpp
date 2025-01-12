2ff7d3ddd1f75589f5212e32e8145877426db059
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-01-12 09:59:38 +0530

    Time: 506 ms (0.00%) | Memory: 227.5 MB (0.00%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -30,7 +30,44 @@ int dp[501][501][3];
     int maximumAmount(vector<vector<int>>& coins) {
 
         m = coins.size(), n = coins[0].size();
-        memset(dp,-1,sizeof(dp));
-        return memo(coins, m - 1, n - 1, 2);
+        // memset(dp,-1,sizeof(dp));
+        // return memo(coins, m - 1, n - 1, 2);
+
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(3,INT_MIN)));
+
+
+        //base case
+        dp[0][0][0]=coins[0][0];
+          if (coins[0][0]<0) {
+            //defend kr lenge
+            dp[0][0][1]=0; 
+            dp[0][0][2]=0;
+        } else {
+            dp[0][0][1]=dp[0][0][2]=coins[0][0];
+        }
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<3;k++){
+                    if(i==0 && j==0) continue;
+
+                    if(i>0){
+                        dp[i][j][k]=max(dp[i][j][k],dp[i-1][j][k]+coins[i][j]);
+                        if(coins[i][j]<0 && k>0){
+                            dp[i][j][k]= max(dp[i][j][k],dp[i-1][j][k-1]);
+                        }
+                    }
+
+                    if(j>0){
+                        dp[i][j][k]=max(dp[i][j][k],dp[i][j-1][k]+coins[i][j]);
+                        if(coins[i][j]<0 && k>0){
+                            dp[i][j][k]= max(dp[i][j][k],dp[i][j-1][k-1]);
+                        }
+                    }
+                }
+            }
+        }
+
+        return max({dp[m-1][n-1][0],dp[m-1][n-1][1],dp[m-1][n-1][2]});
     }
 };
