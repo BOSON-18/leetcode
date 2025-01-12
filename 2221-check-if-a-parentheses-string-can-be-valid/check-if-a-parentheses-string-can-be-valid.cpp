2b1e0fa9a83a15b6b0baf271b7a181e95be56c47
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-01-12 17:25:53 +0530

    Time: 9 ms (85.79%) | Memory: 30.2 MB (53.05%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -4,26 +4,42 @@ public:
         int n=s.length();
         if(n%2!=0) return false;
 
-        stack<int> open,openClose;
+
+        // O(1) space complexity
+        int open=0,close=0;
 
         for(int i=0;i<n;i++){
-            if(locked[i]=='0'){
-                openClose.push(i);
-            }else if(s[i]=='('){
-                open.push(i);
-            }else if(s[i]==')'){
-                if(!open.empty()) open.pop();
-                else if(!openClose.empty()) openClose.pop();
-                else return false;
-            }
+            if(s[i]=='(' || locked[i]=='0') open++;
+            else if(s[i]==')') open--;
+            if(open<0) return false;
+            if(s[n-1-i]==')' || locked[n-1-i]=='0') close++;
+            else if(s[n-1-i]=='(') close--;
+            if(close<0) return false;
         }
 
-        while(!open.empty() && !openClose.empty() && open.top()<openClose.top()){
-            open.pop();
-            openClose.pop();
-        }
+        return true;
 
-        return open.empty();
+    //O(N) space leli
+        // stack<int> open,openClose;
+
+        // for(int i=0;i<n;i++){
+        //     if(locked[i]=='0'){
+        //         openClose.push(i);
+        //     }else if(s[i]=='('){
+        //         open.push(i);
+        //     }else if(s[i]==')'){
+        //         if(!open.empty()) open.pop();
+        //         else if(!openClose.empty()) openClose.pop();
+        //         else return false;
+        //     }
+        // }
+
+        // while(!open.empty() && !openClose.empty() && open.top()<openClose.top()){
+        //     open.pop();
+        //     openClose.pop();
+        // }
+
+        // return open.empty();
         
     }
 };
