21478feea3d83c56d6a602735f6856576395908d
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-02-01 14:12:05 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (25.00%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -3,7 +3,7 @@ public:
     bool isArraySpecial(vector<int>& nums) {
 
         for(int i=1;i<nums.size();i++){
-            if(nums[i-1]%2 == nums[i]%2) return false;
+            if((nums[i-1]&1) == (nums[i]&1)) return false;
         }
         return true;
         
