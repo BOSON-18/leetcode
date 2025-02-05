bc3d506676b3020d5e37760164296d4e68b68113
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-02-05 12:21:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (97.48%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        if (s1 == s2) {
-            return true;
-        }
-        int s1FrequencyMap[26] = {0};
-        int s2FrequencyMap[26] = {0};
+        int firstIndexDiff = 0;
+        int secondIndexDiff = 0;
         int numDiffs = 0;
-
         for (int i = 0; i < s1.size(); i++) {
-            char s1Char = s1[i];
-            char s2Char = s2[i];
-
-            if (s1Char != s2Char) {
+            if (s1[i] != s2[i]) {
                 numDiffs++;
                 // numDiffs is more than 2, one string swap will not make two
                 // strings equal
-                if (numDiffs > 2) return false;
+                if (numDiffs > 2)
+                    return false;
+                else if (numDiffs == 1) {
+                    // store the index of first difference
+                    firstIndexDiff = i;
+                } else {
+                    // store the index of second difference
+                    secondIndexDiff = i;
+                }
             }
-
-            // increment frequencies
-            s1FrequencyMap[s1Char - 'a']++;
-            s2FrequencyMap[s2Char - 'a']++;
         }
-
-        // check if frequencies are equal
-        for (int i = 0; i < 26; i++) {
-            if (s1FrequencyMap[i] != s2FrequencyMap[i]) return false;
-        }
-
-        return true;
+        // check if swap is possible
+        return s1[firstIndexDiff] == s2[secondIndexDiff] &&
+               s1[secondIndexDiff] == s2[firstIndexDiff];
     }
 };
