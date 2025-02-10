90237b9ca65b56c1ef8f318c103ce5c3d3d688d3
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-02-10 23:13:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (87.14%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+
+        int leftIndex=0;
+
+        string ans="";
+
+        for(int i=0;i<s.size();i++){
+            if(isalpha(s[i])){
+                leftIndex=i;
+                ans.push_back(s[i]);
+            }
+            else if(isdigit(s[i])){
+                ans.pop_back();
+                if(i<s.size()-1)
+                leftIndex=max(leftIndex-1,i+1);
+
+            }
+        }
+        return ans;
+    }
+};
