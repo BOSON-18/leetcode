a9bfb39b4ed9d9315f717ef6fc9e9d9b448a4dbf
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-02-14 13:20:21 +0530

    Time: 5 ms (99.21%) | Memory: 78 MB (77.84%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,34 +1,34 @@
 class ProductOfNumbers {
 public:
-vector<int> arr;
-vector<int> prefix;
+    vector<int> nums; //cumulative prod store karenge
+    int n;
+
     ProductOfNumbers() {
-        
+        nums.clear();
+        n = 0;
     }
     
     void add(int num) {
-        arr.push_back(num);
-        for(int i=0;i<arr.size()-1;i++){
-            arr[i]*=num;
+        if(num == 0) {
+            nums = {};
+            n = 0;
+        } else {
+            if(nums.empty()) {
+                nums.push_back(num);
+            } else {
+                nums.push_back(nums[n-1] * num);
+            }
+            n++; //increasing size by 1
         }
     }
     
     int getProduct(int k) {
+        if(k > n) {
+            return 0;
+        } else if(k == n) {
+            return nums[n-1];
+        }
 
-        int n=arr.size();
-        // int product=1;
-        // for(int i=n-1;i>=n-k;i--){
-        //     product*=arr[i];
-        // }
-        // return product;
-
-        return arr[n-k];
+        return nums[n-1] / nums[n-k-1];
     }
 };
-
-/**
- * Your ProductOfNumbers object will be instantiated and called as such:
- * ProductOfNumbers* obj = new ProductOfNumbers();
- * obj->add(num);
- * int param_2 = obj->getProduct(k);
- */
