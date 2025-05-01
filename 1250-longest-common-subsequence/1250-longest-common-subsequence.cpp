a35864b90e276f2075c198a097a248987a233a36
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-05-01 21:41:05 +0530

    Time: 27 ms (55.44%), Space: 27.5 MB (50.63%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -16,8 +16,23 @@ public:
         return dp[index1][index2]= max(match,notMatch);
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1,-1));
-        return memo(text1,text2, text1.size()-1, text2.size()-1,dp);
+        int n=text1.size();
+        int m=text2.size();
+        // vector<vector<int>> dp(text1.size(),vector<int>(text2.size(),-1));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        for(int index1=1;index1<=n;index1++){
+            for(int index2=1;index2<=m;index2++){
+                if(text1[index1-1]==text2[index2-1]){
+                    dp[index1][index2]=1+dp[index1-1][index2-1];
+                }else{
+                    dp[index1][index2]=max(dp[index1][index2-1],dp[index1-1][index2]);
+                }
+            }
+        }
+
+        return dp[n][m];
+        // return memo(text1,text2, text1.size()-1, text2.size()-1,dp);
         
     }
 };
