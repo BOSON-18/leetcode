57254c61f81ab3827a6ebd06f965c43b68119bb6
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-05-02 21:26:52 +0530

    Time: 23 ms (34.29%), Space: 27.3 MB (12.86%) - LeetHub

---
+++ 0868-push-dominoes/0868-push-dominoes.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        int n=dominoes.size();
+        vector<int> left(n,0),right(n,0);
+
+        int force=0;
+        for(int i=n-1;i>=0;i--){
+            char ch=dominoes[i];
+
+            if(ch=='.' && force!=0){
+                left[i]=force;
+                force++;
+            }else if(ch=='L'){
+                // left[i]=0;
+                force=1;
+            }else{
+                force=0;
+            }
+        
+        }
+        force=0;
+        for(int i=0;i<n;i++){
+            char ch=dominoes[i];
+
+            if(ch=='.' && force!=0){
+                right[i]=force;
+                force++;
+            }else if(ch=='R'){
+                force=1;
+            }else{
+                force=0;
+            }
+        
+        }
+
+        string ans="";
+
+        for(int i=0;i<n;i++){
+            if(left[i]==right[i]) ans.push_back(dominoes[i]);
+            else if(right[i]==0 && left[i]!=0) ans.push_back('L');
+            else if(left[i]==0 && right[i]!=0) ans.push_back('R');
+            else if(left[i]>0 && right[i]>0 && left[i]<right[i]) ans.push_back('L');
+            else ans.push_back('R');
+                 
+        }
+    // cout<<"Left: ";
+    //     for(int i:left){
+    //         cout<<i<<" ";
+    //     }
+    //     cout<<endl;
+    //     cout<<"Right: ";
+    //     for(int i:right){
+    //         cout<<i<<" ";
+    //     }
+
+        return ans;
+    }
+};
