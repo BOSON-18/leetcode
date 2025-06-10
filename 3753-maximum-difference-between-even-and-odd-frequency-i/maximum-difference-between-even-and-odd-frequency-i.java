62c4852b0a3f661b8da792c1b6896e68a9c7b58f
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-06-10 22:41:10 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (81.01%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.java
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.java
@@ -3,19 +3,22 @@ class Solution {
     public int maxDifference(String s) {
 
         HashMap<Character, Integer> mpp = new HashMap<>();
+        int[] arr = new int[26];
 
         for (char c : s.toCharArray()) {
-            mpp.put(c, mpp.getOrDefault(c, 0) + 1);
+            // mpp.put(c, mpp.getOrDefault(c, 0) + 1);
+            arr[c-'a']=arr[c-'a']+1;
         }
 
         int odd = 0;
         int even = Integer.MAX_VALUE;
 
-        for (Map.Entry<Character, Integer> it : mpp.entrySet()) {
-            int freq = it.getValue();
+        // for (Map.Entry<Character, Integer> it : mpp.entrySet()) {
+       for(int it:arr){
+            int freq = it;
             if (freq % 2 == 1 && freq > odd) {
                 odd = freq;
-            } else if (freq % 2 == 0 && freq < even) {
+            } else if (freq % 2 == 0 && freq < even && freq!=0) {
                 even = freq;
             }
         }
