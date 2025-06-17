c0c21b7f0a3487ef140ab658808e1f29ec4cd8d2
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-06-17 20:00:13 +0530

    Time: 11 ms (23.28%) | Memory: 20 MB (48.27%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,43 +1,49 @@
 class Solution {
-public:
+    private:
+    bool bfs(int courses,unordered_map<int,vector<int>>& adj,vector<int>& indegree){
+        queue<int> q;
+        int count=0;
 
-bool isCycle(int val,unordered_map<int,vector<int>>& adj,vector<bool>& vis,vector<bool>& isRecursion){
+        for(int i=0;i<courses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+                count++;
+            }
+        }
 
-    vis[val]=true;
-    isRecursion[val]=true;
+        while(!q.empty()){
+            int top=q.front();
+            q.pop();
 
-    for(auto it:adj[val]){
+            for(int i:adj[top]){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    q.push(i);
+                    count++;
+                }
+            }
+            
+        }
 
-        if(isRecursion[it]==true) return true;
-        else if(!vis[it] && isCycle(it,adj,vis,isRecursion)) return true;
+        if(courses==count) return true;
+        return false;
+        
     }
-    isRecursion[val]=false;
-    return false;
-}
+public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // we see pre-requisites we use topo/kahnssssssss
-        // so basically if cycle detected we say its not possible and if its a
-        // DAG we say ye we can study all  subjects SADDDDD T_T
 
-        vector<bool> vis(numCourses,0);
-        vector<bool> inRecursion(numCourses,0);
-
+        vector<int> indegree(numCourses,0);
         unordered_map<int,vector<int>> mpp;
 
-        for(auto it:prerequisites){
-            int u=it[0];
-            int v=it[1];
-
+        for(auto& it: prerequisites){
+            int u=it[0],v=it[1];
+            indegree[u]++;
             mpp[v].push_back(u);
         }
 
+        return bfs(numCourses,mpp,indegree);
 
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i] && isCycle(i,mpp,vis,inRecursion)) return false;
-        }
-
-        return true;
-
-
+        
+        
     }
 };
