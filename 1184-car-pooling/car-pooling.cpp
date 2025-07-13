6065cfe2ca037baea43110d7be8680f51a8c1ea7
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2025-07-13 18:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (42.77%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-
-static bool customComparison(vector<int> a, vector<int> b)
-{
-    
-    return a[1] < b[1]; 
-}
     bool carPooling(vector<vector<int>>& trips, int capacity) {
 
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        sort(begin(trips),end(trips),customComparison);
+        int arr[1001]={0};
 
-        for (auto& it : trips) {
-            int passenger = it[0], pickup = it[1], drop = it[2];
-
-            while (!pq.empty()) {
-                pair<int, int> top = pq.top();
-                int e = top.first;
-                int p = top.second;
-
-                if (e <= pickup) {
-                    capacity += p;
-                    pq.pop();
-                } else
-                    break;
-            }
-
-            if (capacity < passenger)
-                return false;
-
-            capacity -= passenger;
-            pq.push({drop,passenger});
+        for(auto& it:trips){
+            arr[it[1]] += it[0];
+            arr[it[2]] -=it[0];
         }
+
+        int curr=0;
+        for(int& i: arr){
+            curr+=i;
+            if(curr>capacity) return false;
+        }
+
         return true;
+        
     }
 };
