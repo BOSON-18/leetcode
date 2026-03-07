3c8f1a395ee98c2d9f24f3bf49c2956de6b4b672
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2026-03-07 06:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (64.24%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        int cnt1 = 0, maj1 = INT_MIN, cnt2 = 0, maj2 = INT_MIN;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == maj1)
-                cnt1++;
-            else if (nums[i] == maj2)
-                cnt2++;
-            else if (cnt1 == 0) {
-                maj1 = nums[i];
-                cnt1 = 1;
-            } else if (cnt2 == 0) {
-                maj2 = nums[i];
-                cnt2 = 1;
-            } else {
+        int n = nums.size();
+
+        int cnt1 = 0,el1=INT_MIN,cnt2=0,el2=INT_MIN;
+
+        for(int i=0;i<n;i++){
+            int curr= nums[i];
+
+            if(curr == el1) cnt1++;
+            else if(curr == el2) cnt2++;
+            else if(cnt1==0){
+                el1=curr;
+                cnt1=1;
+            }else if(cnt2==0){
+                el2=curr;
+                cnt2=1;
+            }else{
                 cnt1--;
                 cnt2--;
             }
@@ -25,13 +27,13 @@ public:
         cnt1=0,cnt2=0;
 
         for(int it:nums){
-            if(it==maj1) cnt1++;
-            if(it==maj2) cnt2++;
+            if(it==el1) cnt1++;
+            if(it==el2) cnt2++;
         }
 
-        if(cnt1>n/3) ans.push_back(maj1);
-        if(cnt2>n/3) ans.push_back(maj2);
-
+        if(cnt1>n/3) ans.push_back(el1);
+        if(cnt2>n/3) ans.push_back(el2);
+        
         return ans;
     }
 };
