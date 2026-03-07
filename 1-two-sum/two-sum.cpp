d0a60aebd75f29f34f21df48c41718000879ffee
Deeshank Batra <111277323+BOSON-18@users.noreply.github.com>
2026-03-07 21:06:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (18.25%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,30 +2,20 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // return indices
-        //nums[ind1]+nums[ind2]==target
-        //ind1!=ind2
+        // cant sort as sorting will change the indices of the element
 
-        //Brute Force two for loops
-        //Time Complexity O(N^2)
+        // brute force is O(N^2) 2 frr loop kaam khtm
 
-        //Optimise it 
-        //O(NLogN) or O(N)
+        // better map bnake dekh lo
 
-        //Better O(NlogN)
-        //but it will distort the indexes so cant do it
-        //Instead Use Space -> Set 
+        unordered_map<int, int> mpp;
+        int n = nums.size();
+       for(int i=0;i<n;i++){
+        int req = target-nums[i];
+        if(mpp.find(req)!=mpp.end()) return {i,mpp[req]};
+        mpp[nums[i]]=i;
+       }
 
-        unordered_map<int,int> mpp;
-    int n=nums.size();
-        for(int i=0;i<n;i++){
-            int T=target-nums[i];
-            if(mpp.find(T)!=mpp.end()) return {i,mpp[T]};
-
-            mpp[nums[i]]=i;
-        }
-
-        return {-1,-1};
-       
-        
+       return {-1,-1};
     }
 };
